e1d67fbc6deba544cd2a116244cf2f853f6bbe82
Baskoibanes <vovazms@gmail.com>
2023-10-23 15:40:59 +0300

    ALPHA

---
+++ web/app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, session
+import os
+from werkzeug.utils import secure_filename
+
+# WSGI Application
+# Defining upload folder path
+UPLOAD_FOLDER = os.path.join('static', 'uploads')
+# # Define allowed files
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+
+# Configure upload folder for Flask application
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Define secret key to enable session
+app.secret_key = 'This is your secret key to utilize session in Flask'
+
+if __name__=='__main__':
+    app.run(debug = True)
+
+
+@app.route('/')
+def index():
+    return render_template('index_upload_and_display_image.html')
+
+
+@app.route('/',  methods=("POST", "GET"))
+def uploadFile():
+    if request.method == 'POST':
+        # Upload file flask
+        uploaded_img = request.files['uploaded-file']
+        # Extracting uploaded data file name
+        img_filename = secure_filename(uploaded_img.filename)
+        # Upload file to database (defined uploaded folder in static path)
+        uploaded_img.save(os.path.join(app.config['UPLOAD_FOLDER'], img_filename))
+        # Storing uploaded file path in flask session
+        session['uploaded_img_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'], img_filename)
+
+        return render_template('index_upload_and_display_image_page2.html', user_image=session['uploaded_img_file_path'])
+
+
+@app.route('/show_image')
+def displayImage():
+    # Retrieving uploaded file path from session
+    img_file_path = session.get('uploaded_img_file_path', None)
+    # Display image in Flask application web page
+    return render_template('show_image.html', user_image = img_file_path)
+
+
+
+
+
